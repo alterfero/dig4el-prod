20995e7a14047da4ef0eaf2ac11b6ac368bf8d66
Alterfero <alterfero@gmail.com>
2025-05-23 14:57:27 -1000

    selectable dataframes enabling access change and deletion of author's documents.

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+
 def main():
     init_db()  # Initialize the databases
     if 'logged_in' not in st.session_state:
@@ -43,6 +44,8 @@ def main():
 
         # Dashboard
         with st.expander("Consult available documents"):
+
+            # CQ
             session = get_session(os.environ["CQ_DATABASE_URL"])
             if st.session_state.is_guest:
                 cq_data = session.query(CQ).filter(
@@ -54,61 +57,64 @@ def main():
                     | (CQ.access_authorization == "Can be read by other registered users")
                     | (CQ.access_authorization == "Can be read by unregistered guests")
                 ).all()
-
+            c = 0
             st.subheader("Conversational Questionnaires")
             cq_data_list = []
             for cq in cq_data:
                 author_name = (
-                    f"{cq.author.first_name or ''} {cq.author.last_name or ''}"
-                ).strip() or cq.author.username
+                                  f"{cq.author.first_name or ''} {cq.author.last_name or ''}"
+                              ).strip() or cq.author.username
                 cq_data_list.append(
                     {
                         "title": cq.json_data.get("title", "no title"),
                         "author": author_name,
                         "last update": cq.last_update_date,
                         "access": cq.access_authorization,
+                        "author_id": cq.author_id,
+                        "filename": cq.filename
                     }
                 )
-            st.dataframe(pd.DataFrame(cq_data_list))
-
-            # Allow editing or removing if the logged-in user is the author
-            for cq in cq_data:
-                if cq.author_id == st.session_state.user_id:
-                    with st.expander(
-                        f"Manage CQ: {cq.json_data.get('title', 'no title')}",
-                        expanded=False,
-                    ):
-                        access_options = [
-                            "No sharing",
-                            "Can be read by other registered users",
-                            "Can be read by unregistered guests",
-                        ]
-                        access_index = access_options.index(cq.access_authorization)
-                        new_access = st.selectbox(
-                            "Access Authorization",
-                            access_options,
-                            index=access_index,
-                            key=f"cq_access_{cq.id}",
-                        )
-                        if st.button("Update Access", key=f"update_cq_{cq.id}"):
-                            update_session = get_session(os.environ["CQ_DATABASE_URL"])
-                            item = update_session.query(CQ).get(cq.id)
-                            item.access_authorization = new_access
-                            update_session.commit()
-                            update_session.close()
-                            st.success("Access updated")
-                            st.rerun()
-                        if st.button("Delete", key=f"delete_cq_{cq.id}"):
-                            delete_session = get_session(os.environ["CQ_DATABASE_URL"])
-                            item = delete_session.query(CQ).get(cq.id)
-                            delete_session.delete(item)
-                            delete_session.commit()
-                            delete_session.close()
-                            st.success("CQ removed")
-                            st.rerun()
+            cq_df = pd.DataFrame(cq_data_list)
+            selected_cq = st.dataframe(cq_df, on_select="rerun",
+                                       selection_mode="single-row",
+                                       column_config={"author_id":None, "filename":None})
+            # Allow editing or removing the selected document if the logged-in user is the author
+            if selected_cq["selection"]["rows"] != []:
+                if cq_df.iloc[selected_cq["selection"]["rows"][0]]["author_id"] == \
+                        st.session_state.user_id:
+                    current_cq = [cq for cq in cq_data if cq.filename == cq_df.iloc[selected_cq["selection"]["rows"][0]]["filename"]][0]
+                    access_options = [
+                        "No sharing",
+                        "Can be read by other registered users",
+                        "Can be read by unregistered guests",
+                    ]
+                    access_index = access_options.index(cq.access_authorization)
+                    new_access = st.selectbox(
+                        "Change Access Authorization",
+                        access_options,
+                        index=access_index,
+                        key=f"cq_access_{cq.id}",
+                    )
+                    if st.button("Update Access", key=f"update_cq_{cq.id}"):
+                        update_session = get_session(os.environ["CQ_DATABASE_URL"])
+                        item = update_session.query(CQ).get(cq.id)
+                        item.access_authorization = new_access
+                        update_session.commit()
+                        update_session.close()
+                        st.success("Access updated")
+                        st.rerun()
+                    if st.button("Delete", key=f"delete_cq_{cq.id}"):
+                        delete_session = get_session(os.environ["CQ_DATABASE_URL"])
+                        item = delete_session.query(CQ).get(cq.id)
+                        delete_session.delete(item)
+                        delete_session.commit()
+                        delete_session.close()
+                        st.success("CQ removed")
+                        st.rerun()
 
             session.close()
 
+            # TRANSCRIPTION
             session = get_session(os.environ["TRANSCRIPTION_DATABASE_URL"])
             if st.session_state.is_guest:
                 transcription_data = session.query(Transcription).filter(
@@ -137,13 +143,12 @@ def main():
                 ).all()
             session_lcq = session
 
-
             st.subheader("Transcriptions")
             transcription_data_list = []
             for transcription in transcription_data:
                 guardian_name = (
-                    f"{transcription.author.first_name or ''} {transcription.author.last_name or ''}"
-                ).strip() or transcription.author.username
+                                    f"{transcription.author.first_name or ''} {transcription.author.last_name or ''}"
+                                ).strip() or transcription.author.username
                 transcription_data_list.append(
                     {
                         "Guardian": guardian_name,
@@ -151,58 +156,67 @@ def main():
                         "Interviewer": transcription.json_data.get("interviewer", "unknown"),
                         "last update": transcription.last_update_date,
                         "access": transcription.access_authorization,
+                        "author_id": transcription.author_id,
+                        "filename": transcription.filename
                     }
 
                 )
-            st.dataframe(pd.DataFrame(transcription_data_list))
 
-            for transcription in transcription_data:
-                if transcription.author_id == st.session_state.user_id:
-                    with st.expander(
-                        f"Manage Transcription: {transcription.filename}",
-                        expanded=False,
-                    ):
-                        access_options = [
-                            "No sharing",
-                            "Can be read by other registered users",
-                            "Can be read by unregistered guests",
-                        ]
-                        access_index = access_options.index(
-                            transcription.access_authorization
-                        )
-                        new_access = st.selectbox(
-                            "Access Authorization",
-                            access_options,
-                            index=access_index,
-                            key=f"trans_access_{transcription.id}",
-                        )
-                        if st.button(
+            transcription_df = pd.DataFrame(transcription_data_list)
+            selected_transcription = st.dataframe(transcription_df, on_select="rerun",
+                                       selection_mode="single-row",
+                                       column_config={"author_id": None, "filename": None})
+
+            # Allow editing or removing the selected document if the logged-in user is the author
+            if selected_transcription["selection"]["rows"] != []:
+                if transcription_df.iloc[selected_transcription["selection"]["rows"][0]]["author_id"] == \
+                        st.session_state.user_id:
+                    current_transcription = [transcription for transcription in transcription_data if
+                                  transcription.filename == transcription_df.iloc[selected_transcription["selection"]["rows"][0]]["filename"]][0]
+                    access_options = [
+                                    "No sharing",
+                                    "Can be read by other registered users",
+                                    "Can be read by unregistered guests",
+                                ]
+                    access_index = access_options.index(
+                        transcription.access_authorization
+                    )
+                    new_access = st.selectbox(
+                        "Change Access Authorization",
+                        access_options,
+                        index=access_index,
+                        key=f"trans_access_{transcription.id}",
+                    )
+                    if st.button(
                             "Update Access",
                             key=f"update_trans_{transcription.id}",
-                        ):
-                            update_session = get_session(os.environ["TRANSCRIPTION_DATABASE_URL"])
-                            item = update_session.query(Transcription).get(transcription.id)
-                            item.access_authorization = new_access
-                            update_session.commit()
-                            update_session.close()
-                            st.success("Access updated")
-                            st.rerun()
-                        if st.button("Delete", key=f"delete_trans_{transcription.id}"):
-                            delete_session = get_session(os.environ["TRANSCRIPTION_DATABASE_URL"])
-                            item = delete_session.query(Transcription).get(transcription.id)
-                            delete_session.delete(item)
-                            delete_session.commit()
-                            delete_session.close()
-                            st.success("Transcription removed")
-                            st.rerun()
-            session_transcription.close()
-
+                    ):
+                        update_session = get_session(os.environ["TRANSCRIPTION_DATABASE_URL"])
+                        item = update_session.query(Transcription).get(transcription.id)
+                        item.access_authorization = new_access
+                        update_session.commit()
+                        update_session.close()
+                        st.success("Access updated")
+                        st.rerun()
+                    if st.button("Delete", key=f"delete_trans_{transcription.id}"):
+                        delete_session = get_session(os.environ["TRANSCRIPTION_DATABASE_URL"])
+                        item = delete_session.query(Transcription).get(transcription.id)
+                        delete_session.delete(item)
+                        delete_session.commit()
+                        delete_session.close()
+                        st.success("Transcription removed")
+                        st.rerun()
+                    session_transcription.close()
+                else:
+                    st.write("You must be the guardian of a document to change its attributes.")
             st.subheader("ConQuest: Legacy Conversational Questionnaires")
+
+            # LEGACY TRANSCRIPTION
             lcq_data_list = []
             for lcq in lcq_data:
                 author_name = (
-                    f"{lcq.author.first_name or ''} {lcq.author.last_name or ''}"
-                ).strip() or lcq.author.username
+                                  f"{lcq.author.first_name or ''} {lcq.author.last_name or ''}"
+                              ).strip() or lcq.author.username
                 lcq_data_list.append(
                     {
                         "filename": lcq.filename,
@@ -216,7 +230,7 @@ def main():
             for lcq in lcq_data:
                 if lcq.author_id == st.session_state.user_id:
                     with st.expander(
-                        f"Manage Legacy CQ: {lcq.filename}", expanded=False
+                            f"Manage Legacy CQ: {lcq.filename}", expanded=False
                     ):
                         access_options = [
                             "No sharing",
@@ -293,7 +307,8 @@ def main():
                             confirm_cq = None
 
                         session = get_session(os.environ["TRANSCRIPTION_DATABASE_URL"])
-                        existing = session.query(Transcription).filter(Transcription.filename == uploaded_file.name).order_by(Transcription.version.desc()).first()
+                        existing = session.query(Transcription).filter(
+                            Transcription.filename == uploaded_file.name).order_by(Transcription.version.desc()).first()
                         session.close()
                         if existing:
                             confirm_update = st.radio("File already exists, update it?", ["No", "Yes"], index=0)
@@ -304,7 +319,8 @@ def main():
                         target_language = st.text_input("Target language", value=json_data.get("target language", ""))
                         interviewer = st.text_input("Interviewer name", value=json_data.get("interviewer", ""))
                         consultant = st.text_input("Consultant name", value=json_data.get("consultant", ""))
-                        consultant_auth = st.checkbox("I have authorization from the consultant to upload this transcription")
+                        consultant_auth = st.checkbox(
+                            "I have authorization from the consultant to upload this transcription")
 
                         if st.button("Upload"):
                             if cq_match is None:
@@ -342,7 +358,8 @@ def main():
                     if uploaded_file and upload_access_authorization:
                         if upload_type == "Legacy CQ":
                             session = get_session(os.environ["LCQ_DATABASE_URL"])
-                            existing = session.query(LegacyCQ).filter(LegacyCQ.filename == uploaded_file.name).order_by(LegacyCQ.version.desc()).first()
+                            existing = session.query(LegacyCQ).filter(LegacyCQ.filename == uploaded_file.name).order_by(
+                                LegacyCQ.version.desc()).first()
                             if existing:
                                 confirm_update_lcq = st.radio("File already exists, update it?", ["No", "Yes"], index=0)
                             else:
@@ -355,7 +372,8 @@ def main():
                                 st.error("Invalid JSON file.")
                                 json_data = None
                             session = get_session(os.environ["CQ_DATABASE_URL"])
-                            existing_cq = session.query(CQ).filter(CQ.filename == uploaded_file.name).order_by(CQ.version.desc()).first()
+                            existing_cq = session.query(CQ).filter(CQ.filename == uploaded_file.name).order_by(
+                                CQ.version.desc()).first()
                             session.close()
                             if existing_cq:
                                 confirm_update_cq = st.radio("File already exists, update it?", ["No", "Yes"], index=0)
@@ -469,7 +487,6 @@ def main():
             else:
                 st.error("Failed to create guest user.")
 
-
         # Registration
         st.header("Register")
         new_username = st.text_input("Username", key="register_username")
@@ -481,13 +498,13 @@ def main():
         certify = st.checkbox("I certify that these information are correct", key="register_certify")
         if st.button("Register"):
             if (
-                new_username
-                and new_email
-                and new_password
-                and new_first_name
-                and new_last_name
-                and new_orcid
-                and certify
+                    new_username
+                    and new_email
+                    and new_password
+                    and new_first_name
+                    and new_last_name
+                    and new_orcid
+                    and certify
             ):
                 if verify_orcid(new_orcid, new_first_name, new_last_name):
                     st.success("ORCID successfully verified")
@@ -523,6 +540,7 @@ def main():
                 st.success("If the email is registered, a temporary password has been sent.")
             else:
                 st.error("Please enter your email address.")
+
 
 if __name__ == "__main__":
     main()
